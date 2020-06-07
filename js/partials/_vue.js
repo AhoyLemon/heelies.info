6c23680921b625a2ce10d8eae8ed5c78baa8d4de
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2020-06-06 21:51:18 -0500

    rotating banner ad

--- js/partials/_vue.js
+++ js/partials/_vue.js
@@ -2,19 +2,39 @@ var app = new Vue({
   el: '#app',
   data: {
     mode: "canon",
-    h1: 'It works.'
+    h1: 'It works.',
+    currentBanner: '',
+    banners: [
+      'soaker-for-bear.png',
+      'ears-4-kids.png'
+    ]
   },
 
   methods: {
+    rotateBanner() {
+      let self = this;
+      console.log('rotate');
+      let f = randomFrom(self.banners);
 
+      if (f != self.currentBanner) {
+        self.currentBanner = f;
+      } else {
+        self.rotateBanner();
+      }
+    },
   },
 
   computed: {
-
   },
 
   mounted: function() {
+    let self = this;
 
+    self.currentBanner = randomFrom(self.banners);
+
+    window.setInterval(function(){
+      self.rotateBanner();
+    }, 5000);
   }
 
 });
